60083fc58925a1b39d0d4f91f775f9fc115e00c7
Katherine <kbd3ax@virginia.edu>
2017-12-20 16:55:54 -0500

    Create basic canvas graphics
    -Simple shapes drawn in a canvas positioned simply in middle of page

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
+/*
+ * Title: "Just Get 10!" JS Game
+ *
+ * By: Katherine Donnally, December 2017
+ *
+ */
 
+(function() {
+	var canvas,
+		ctx;
+
+	canvas = document.getElementById("game-canvas");
+
+	if (ctx = canvas.getContext('2d')) {
+		ctx.fillStyle = "rgba(212,0,255,.5)";
+		ctx.fillRect(50,50,100,100);
+
+		ctx.fillStyle = "rgba(125,0,255,.5)";
+		ctx.fillRect(75,75,100,100);
+	}
+	// fallback
+	else {
+		canvas.innerHTML = "<img src='img/classy-fabric.png' alt='fallback image'>";
+	}
+})();
