bb93f7b83bbf0da07e6d204e125a268f72bd4c8c
Katherine <kbd3ax@virginia.edu>
2017-12-21 16:04:10 -0500

    Change game from canvas to CSS Grid + Hammer.js

--- main.js
+++ main.js
@@ -6,20 +6,56 @@
  */
 
 (function() {
-	var canvas,
-		ctx;
+	var gameParent,
+		gameGrid,
+		items,
+		gameManager;
 
-	canvas = document.getElementById("game-canvas");
+	gameParent = document.querySelector(".game-container--parent");
+	gameGrid = document.querySelector(".game-container__grid");
+	items = document.getElementsByClassName("game-grid__item");
 
-	if (ctx = canvas.getContext('2d')) {
-		ctx.fillStyle = "rgba(212,0,255,.5)";
-		ctx.fillRect(50,50,100,100);
-
-		ctx.fillStyle = "rgba(125,0,255,.5)";
-		ctx.fillRect(75,75,100,100);
+	// generate random number, assign to HTML & attribute
+	for (var i = 0; i < items.length; i++) {
+		var num = Math.floor((Math.random() * 3) + 1);
+		items[i].innerHTML = num;
+		items[i].setAttribute("data-value", num.toString());
 	}
-	// fallback
-	else {
-		canvas.innerHTML = "<img src='img/classy-fabric.png' alt='fallback image'>";
-	}
+
+	// recognize tap, pan, swipe in game container
+	gameManager = new Hammer.Manager(gameGrid, {
+		recognizers: [
+			[Hammer.Tap],
+			[Hammer.Tap,{ event: 'doubletap', taps: 2}],
+			[Hammer.Pan,{ threshold:0 }],
+			[Hammer.Swipe,{ direction: Hammer.DIRECTION_HORIZONTAL }],
+		]
+	});
+
+	// change item style if tapped
+	gameManager.on("tap", function(e) {
+		console.log(e.type);
+		e.target.classList.toggle('item-selected');
+		item = {
+			value: parseInt(e.target.getAttribute('data-value')),
+			row:  parseInt(e.target.getAttribute('data-grid-row')),
+			column:  parseInt(e.target.getAttribute('data-grid-col')),
+			get rowAbove() {
+				return this.row - 1;
+			},
+			get rowBelow() {
+				return this.row + 1;
+			},
+			get rowItems() {
+				return document.querySelectorAll(`[data-grid-row="${this.row}"]`);
+			},
+			get columnItems() {
+				return document.querySelectorAll(`[data-grid-col="${this.column}"]`);
+			},
+		}
+		console.log(item.rowItems);
+		console.log(item.columnItems);
+	});
+
+
 })();
