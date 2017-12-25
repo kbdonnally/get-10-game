cc3a28a18e373947c12ab7a6d79cb637b7163e53
Katherine <kbd3ax@virginia.edu>
2017-12-25 10:51:17 -0500

    IT'S PRESENT TIME ON CHRIMBUS!!! explain changes later

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
  * By: Katherine Donnally, December 2017
  *
  */
-"use strict";
+//"use strict";
 
 (function() {
 	var gameParent,
@@ -13,15 +13,20 @@
 		gameManager;
 
 	gameParent 	= document.querySelector(".game-container--parent");
-	gameGrid 	= document.querySelector(".game-container__grid");
+	gameGrid 	= document.querySelector(".game-container__flex");
 	items 		= document.getElementsByClassName("game-grid__item");
 
-	// take gameGrid           -> enable tap, pan, swipe inside
+	// take gameGrid           -> enable tap, pan in it
 	gameManager = new Hammer.Manager(gameGrid, {
 				recognizers: [
 					[Hammer.Tap,{ interval: 500 }],
 					[Hammer.Pan,{ threshold:0 }]
 				]
+	});
+
+	// manager for entire container
+	var parentManager = new Hammer.Manager(gameParent, {
+		recognizers: [ [Hammer.Tap, { interval: 500 }] ]
 	});
 
 	// on load take items[i]  -> assign int from 1-3
@@ -36,7 +41,7 @@
 		value: 				   parseInt(elem.getAttribute('data-value')),
 		row:  				   parseInt(elem.getAttribute('data-grid-row')),
 		col:  				   parseInt(elem.getAttribute('data-grid-col')),
-		gridAreaFull: 		   elem.style.gridArea
+		position: 			   elem.getAttribute('data-position')
 	});
 
 	/* take attrsTemplate obj  -> props:   {value: n, row: i, col: j, gridArea: ri-cj}
@@ -47,10 +52,12 @@
 			this.value 		   = attrs.value;
 			this.row 		   = attrs.row;
 			this.col 		   = attrs.col;
-			this.gridArea 	   = attrs.gridAreaFull.slice(0,5);
+			this.position	   = attrs.position;
 		}
-		get itemsSameVal() {
-							   return document.querySelectorAll(`[data-value="${this.value}"]`);
+		get itemsSameVal() { 
+							   let elems = document.querySelectorAll(`[data-value="${this.value}"]`);
+							   return Array.prototype.filter.call(elems, elem => 
+							   	parseInt(elem.getAttribute('data-grid-row')) > 9);
 		}
 		get neighbors() {
 							   return [
@@ -62,102 +69,214 @@
 		}
 	}
 
-	// tap handler
+
+	// deselect if tap outside game region
+	parentManager.on("tap", function(e) {
+		if (e.target.classList.contains('game-grid__item') == false) {
+			for (let child of document.querySelectorAll('.item-selected')) {
+				child.classList.remove('item-selected');
+			}
+		}
+	});
+
+	// handle game interactions
 	gameManager.on("tap", function(e) {
 		console.log(e.type);
 		console.log(e.tapCount);
 
-		/* NB: 
-		 * if items already highlighted & they're a diff value, deselect them
-		 * if same value and already highlighted, squish them together and up the value
-		 */
+		
+		// CASE: there are already selected items present
 		if (document.querySelectorAll('.item-selected').length != 0) {
-			if (document.querySelector('.item-selected').innerHTML != e.target.innerHTML) {
-				for (let child of gameGrid.children) {
+
+			// simple delay function
+			function delay(t) {
+			   return new Promise(function(resolve) { 
+			       setTimeout(resolve, t)
+			   });
+			}
+
+			// x,y translate for each item
+			function moveDown(child) {
+				var childNums = attrsTemplate(child);
+				var targetNums = attrsTemplate(e.target);
+				var s = parseInt(e.target.offsetWidth);
+				var changeX = (targetNums.col - childNums.col) * (s) + "px";
+				var changeY = (targetNums.row - childNums.row) * (s) + "px";
+
+				return new Promise(function(resolve, reject) {
+					child.style.transform = `translate(${changeX}, ${changeY})`;
+
+					if (child.style.transform == `translate(${changeX}, ${changeY})`) {
+						resolve(child);
+					}
+					else {
+						reject(Error(child.style));
+					}
+				});
+			}
+
+			// CASE: e.target is not in the group of selected items
+			if (document.querySelector('.item-selected').innerHTML != e.target.innerHTML ||
+				Array.prototype.includes.call(document.querySelectorAll('.item-selected'), e.target) == false) {
+				for (let child of document.querySelectorAll('.item-selected')) {
 					child.classList.remove('item-selected');
 				}
 			}
-			else {
+
+			// CASE: e.target is in group of selected items
+			else if (Array.prototype.includes.call(document.querySelectorAll('.item-selected'), e.target)) {
+				// increment target
 				e.target.innerHTML = parseInt(e.target.innerHTML) + 1;
 				e.target.setAttribute('data-value', e.target.innerHTML);
-				for (let child of gameGrid.children) {
-					if (child != e.target && child.classList.contains('item-selected')) {
-					//	child.innerHTML = "test";
-						child.remove();
+				e.target.classList.remove('item-selected');
+
+				// all items except target:
+				let selected = document.querySelectorAll('.item-selected');
+				// PROMISES:
+				Promise.all(selected).then( (items) => {
+					for (let item of items) {
+						moveDown(item);
 					}
+					return items;
+				}).then( (items) => {
+					return delay(1000).then( () => {
+						for (let child of items) {
+							child.parentNode.insertBefore(child, child.parentNode.firstChild);
+						}
+						return items;
+					});
+				}).then ( (items) => {
+					for (let child of items) {
+						let num = Math.floor((Math.random() * 3) + 1);
+						child.innerHTML = num;
+						child.setAttribute("data-value", num.toString());
+						child.classList.remove('item-selected');
+						child.style.transform = "translate(0,0)";
+					}
+					return items;
+				}).then( (items) => {
+					return delay(100).then( () => {
+						let columns = document.querySelectorAll('[class*="game-flex__col--"]');
+						Promise.all(columns).then( (columns) => {
+							for (let column of columns) {
+								for (let item of column.children) {
+									item.style.order = Array.prototype.indexOf.call(column.children, item);
+									item.setAttribute('data-grid-row', item.style.order);
+								}
+							}
+							return columns;
+						});
+					});
+				}).catch( (error) => console.log(error));
+				/* MOVE THIS: 
+				e.target.innerHTML = parseInt(e.target.innerHTML) + 1;
+				e.target.setAttribute('data-value', e.target.innerHTML); */
+			/*	for (let child of document.querySelectorAll('.item-selected')) {
 					child.classList.remove('item-selected');
-				}	
+					if (child != e.target) {
+						/* insert x/y transitions here! */
+				/*		var childNums = attrsTemplate(child);
+						var targetNums = attrsTemplate(e.target);
+						var s = parseInt(e.target.offsetWidth);
+						var changeX = (targetNums.col - childNums.col) * (s) + "px";
+						var changeY = (targetNums.row - childNums.row) * (s) + "px";
+					//	child.style.transform = `translate(${changeX}, ${changeY})`; */
+
+						// PROMISES!!!
+
+						
+
+					/*	moveDown(child).then( function(div) {
+							console.log("test");
+							return delay(1000).then( function() {
+								div.style.order = -8;
+								div.style.backgroundColor = "purple";
+								console.log("another test");
+								return div;
+							});
+						}).then( function(div) {
+							return delay(1000).then( function() {
+								console.log("un-transform!");
+								div.style.transform = "translate(0,0)";
+								return div;
+							});
+						}).catch(function(error) {
+							console.log(error);
+						}); */
+					
+
+
+
+						/* now move elems up */
+					//	child.parentNode.insertBefore(child, child.parentNode.firstChild);
+					//	child.style.transform = "translate(0,0)";
+						// reset value
+					/*	let num = Math.floor((Math.random() * 3) + 1);
+						child.innerHTML = num;
+						child.setAttribute("data-value", num.toString()); */
+				
+			/*	for (let column of document.querySelectorAll('[class*="game-flex__col--"]')) {
+
+					for (let item of column.children) {
+						item.style.order = Array.prototype.indexOf.call(column.children, item);
+						item.setAttribute('data-grid-row', item.style.order);
+					}
+				} */
+			}
+			else {
+				console.log('This happened?');
 			}
 		}
 
-		// highlight target
-		e.target.classList.toggle('item-selected');
+		else {
+			// highlight target
+			e.target.classList.toggle('item-selected');
 
-		// make event target a GameItem
-		var tappedItem = new GameItem(attrsTemplate(e.target));
-		console.log(tappedItem);
+			// make event target a GameItem
+			var tappedItem = new GameItem(attrsTemplate(e.target));
+			console.log(tappedItem.itemsSameVal);
 
-		// NodeList of all elems w/ same number as target
-		var sameVal = tappedItem.itemsSameVal;
+			// NodeList of all elems w/ same number as target
+			var sameVal = tappedItem.itemsSameVal;
 
-		// flood fill
-		function highlightNeighbors(array) {
-			var nextRound = [];
-			for (var x=0; x < array.length; x++) {
-				var active = new GameItem(attrsTemplate(array[x]));
-				for (var i=0; i < sameVal.length; i++) {
-					for (var g=0; g < active.neighbors.length; g++) {
-						if (sameVal[i].style.gridArea.slice(0,5) == active.neighbors[g]) {
-							sameVal[i].classList.add('item-selected');
-							nextRound.push(sameVal[i]);
+			// define flood fill
+			function highlightNeighbors(array) {
+				var nextRound = [];
+				for (var x=0; x < array.length; x++) {
+					var active = new GameItem(attrsTemplate(array[x]));
+					for (var i=0; i < sameVal.length; i++) {
+						for (var g=0; g < active.neighbors.length; g++) {
+							if (sameVal[i].getAttribute('data-position') == active.neighbors[g]) {
+								sameVal[i].classList.add('item-selected');
+								nextRound.push(sameVal[i]);
+							}
 						}
 					}
 				}
+				if (nextRound.length == 0) {
+					e.target.classList.remove('item-selected');
+				}
+				return nextRound;
 			}
-			if (nextRound.length == 0) {
-				e.target.classList.remove('item-selected');
+
+			// reset all row numbers
+			for (let column of document.querySelectorAll('[class*="game-flex__col--"]')) {
+				for (let item of column.children) {
+					item.style.order = Array.prototype.indexOf.call(column.children, item);
+					item.setAttribute('data-grid-row', item.style.order);
+					let col = item.getAttribute('data-grid-col');
+					item.setAttribute('data-position', `r${item.style.order}-c${col}`);
+				}
 			}
-			return nextRound;
+			
+			// execute flood fill
+			var roundTwo = highlightNeighbors([e.target]);
+			var roundThree = highlightNeighbors(roundTwo);
+			var roundFour = highlightNeighbors(roundThree);
+			var roundFive = highlightNeighbors(roundFour);
+			var roundSix = highlightNeighbors(roundFive);
+			var roundSeven = highlightNeighbors(roundSix);
 		}
 		
-		var roundTwo = highlightNeighbors([e.target]);
-		var roundThree = highlightNeighbors(roundTwo);
-		var roundFour = highlightNeighbors(roundThree);
-		var roundFive = highlightNeighbors(roundFour);
-		var roundSix = highlightNeighbors(roundFive);
-
-		/* Kristina's math school: make a loop for 25 of them ffs */
-
-		/*
-
-		// highlight next neighbors
-		if (roundTwo.length > 0) {
-			console.log(roundTwo);
-			for (var i=0; i < roundTwo.length; i++) {
-				console.log(roundTwo[i]);
-				var active = new GameItem(attrsTemplate(roundTwo[i]));
-				console.log(active);
-				for (var j=0; j < sameVal.length; j++) {
-					if (sameVal[i].style.gridArea.slice(0,5) == active.)	
-				}
-			}
-		} */
-
-		// highlight neighbors' neighbors
-	/*	for (var i=0; i < sameVal.length; i++) {
-			console.log(roundTwo[i]);
-			var currentItem = new GameItem(attrsTemplate(roundTwo[i]));
-			for (var g=0; g < currentItem.neighbors.length; g++) {
-				if (sameVal[i].style.gridArea.slice(0,5) == currentItem.neighbors[g]) {
-					sameVal[i].classList.add('item-selected');	
-				}
-			}
-		} */
-		
-
-
 	});
-
-	
-
 })();
